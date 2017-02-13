5f5f0ee7e9957c7c181e956221cc8f05814b1f9b
Richard Senior <richard.j.senior@gmail.com>
2017-02-13 08:56:55 +0000

    Added more records to data loading script

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -13,5 +13,110 @@ values ('G-SHOZ', 'Lockheed1049h', 'EGNM', 600, '2017-02-10 16:32:00.000', 0, 'E
 insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
 values ('G-SHOZ', 'Lockheed1049h', 'EGLL', 600, '2017-02-10 18:46:00.000', 0, 'EDDF', 120, '2017-02-10 20:12:00.000', 220, 'COMPLETE');
 
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup150', 'EGCJ', 24, '2017-01-09 10:12:00.000', 0, 'EGNT', 16, '2017-01-09 11:0:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGCJ', 24, '2017-01-10 08:00:00.000', 0, 'EGNT', 18, '2017-01-10 08:40:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGNT', 18, '2017-01-10 09:32:00.000', 0, 'EGNL', 8.7, '2017-01-10 10:40:00.000', 120, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGNM', 600, '2017-01-10 16:32:00.000', 0, 'EGLL', 150, '2017-01-10 17:44:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGLL', 600, '2017-01-10 18:46:00.000', 0, 'EDDF', 120, '2017-01-10 20:12:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup150', 'EGCJ', 24, '2017-02-09 10:12:00.000', 0, 'EGNT', 16, '2017-02-09 11:0:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGCJ', 24, '2017-02-10 08:00:00.000', 0, 'EGNT', 18, '2017-02-10 08:40:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGNT', 18, '2017-02-10 09:32:00.000', 0, 'EGNL', 8.7, '2017-02-10 10:40:00.000', 120, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGNM', 600, '2017-02-10 16:32:00.000', 0, 'EGLL', 150, '2017-02-10 17:44:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGLL', 600, '2017-02-10 18:46:00.000', 0, 'EDDF', 120, '2017-02-10 20:12:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup150', 'EGCJ', 24, '2017-01-09 10:12:00.000', 0, 'EGNT', 16, '2017-01-09 11:0:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGCJ', 24, '2017-01-10 08:00:00.000', 0, 'EGNT', 18, '2017-01-10 08:40:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGNT', 18, '2017-01-10 09:32:00.000', 0, 'EGNL', 8.7, '2017-01-10 10:40:00.000', 120, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGNM', 600, '2017-01-10 16:32:00.000', 0, 'EGLL', 150, '2017-01-10 17:44:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGLL', 600, '2017-01-10 18:46:00.000', 0, 'EDDF', 120, '2017-01-10 20:12:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup150', 'EGCJ', 24, '2017-02-09 10:12:00.000', 0, 'EGNT', 16, '2017-02-09 11:0:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGCJ', 24, '2017-02-10 08:00:00.000', 0, 'EGNT', 18, '2017-02-10 08:40:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGNT', 18, '2017-02-10 09:32:00.000', 0, 'EGNL', 8.7, '2017-02-10 10:40:00.000', 120, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGNM', 600, '2017-02-10 16:32:00.000', 0, 'EGLL', 150, '2017-02-10 17:44:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGLL', 600, '2017-02-10 18:46:00.000', 0, 'EDDF', 120, '2017-02-10 20:12:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup150', 'EGCJ', 24, '2017-01-09 10:12:00.000', 0, 'EGNT', 16, '2017-01-09 11:0:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGCJ', 24, '2017-01-10 08:00:00.000', 0, 'EGNT', 18, '2017-01-10 08:40:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGNT', 18, '2017-01-10 09:32:00.000', 0, 'EGNL', 8.7, '2017-01-10 10:40:00.000', 120, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGNM', 600, '2017-01-10 16:32:00.000', 0, 'EGLL', 150, '2017-01-10 17:44:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGLL', 600, '2017-01-10 18:46:00.000', 0, 'EDDF', 120, '2017-01-10 20:12:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup150', 'EGCJ', 24, '2017-02-09 10:12:00.000', 0, 'EGNT', 16, '2017-02-09 11:0:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGCJ', 24, '2017-02-10 08:00:00.000', 0, 'EGNT', 18, '2017-02-10 08:40:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGNT', 18, '2017-02-10 09:32:00.000', 0, 'EGNL', 8.7, '2017-02-10 10:40:00.000', 120, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGNM', 600, '2017-02-10 16:32:00.000', 0, 'EGLL', 150, '2017-02-10 17:44:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGLL', 600, '2017-02-10 18:46:00.000', 0, 'EDDF', 120, '2017-02-10 20:12:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup150', 'EGCJ', 24, '2017-01-09 10:12:00.000', 0, 'EGNT', 16, '2017-01-09 11:0:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGCJ', 24, '2017-01-10 08:00:00.000', 0, 'EGNT', 18, '2017-01-10 08:40:00.000', 80, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'pup100', 'EGNT', 18, '2017-01-10 09:32:00.000', 0, 'EGNL', 8.7, '2017-01-10 10:40:00.000', 120, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGNM', 600, '2017-01-10 16:32:00.000', 0, 'EGLL', 150, '2017-01-10 17:44:00.000', 220, 'COMPLETE');
+
+insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, destination, end_fuel, end_time, end_odometer, status) 
+values ('G-SHOZ', 'Lockheed1049h', 'EGLL', 600, '2017-01-10 18:46:00.000', 0, 'EDDF', 120, '2017-01-10 20:12:00.000', 220, 'COMPLETE');
+
 insert into flight (callsign, aircraft, origin, start_fuel, start_time, start_odometer, status) 
 values ('G-SHOZ', 'pup150', 'EGNL', 24, '2017-02-11 14:32:00.000', 0, 'ACTIVE');
