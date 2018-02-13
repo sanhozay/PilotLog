c644015feb3922976c719e8d6570dc6b2a6f419e
Richard Senior <richard.j.senior@gmail.com>
2018-02-13 18:18:18 +0000

    Code cleanup in the Angular.js and HTML codebase
     * Group variables into objects
     * Renaming and formatting

--- src/main/resources/static/js/flightrecord/flightrecord.component.js
+++ src/main/resources/static/js/flightrecord/flightrecord.component.js
@@ -2,54 +2,57 @@ angular.module("flightrecord").component("flightrecord", {
     controller: function($http, $interval, $filter) {
         var ctrl = this
         ctrl.$onInit = function() {
-            ctrl.page = 1
-            ctrl.sort = {property: "id", direction: "DESC"}
-            ctrl.example = {}
-            ctrl.form = {}
+            ctrl.pageable = {pageNumber: 1, sort: {property: "id", direction: "DESC"}}
+            ctrl.search = {form: {}, example: {}}
             ctrl.refresh()
             $interval(ctrl.refresh, 1000)
         }
-        ctrl.clickColumn = function(flight, property) {
+        ctrl.columnClicked = function(flight, property) {
             if (property == "startTime") {
                 var route = "from " + flight.origin + " to " + flight.destination
                 var aircraft = $filter('capitalize')(flight.aircraft)
                 if (confirm("Delete " + aircraft + "  flight " + route + "?")) {
-                    $http.delete("/api/flights/flight/" + flight.id).then(ctrl.refresh)
+                    $http.delete("/api/flights/flight/" + flight.id)
+                        .then(ctrl.refresh)
                 }
             } else if (property == "aircraft") {
-                ctrl.form["aircraft"] = $filter('capitalize')(flight["aircraft"])
-                ctrl.example["aircraft"] = $filter('capitalize')(flight["aircraft"])
-                ctrl.gotoPage(1)
+                ctrl.search.form["aircraft"] = $filter('capitalize')(flight["aircraft"])
+                ctrl.search.example["aircraft"] = $filter('capitalize')(flight["aircraft"])
+                ctrl.refreshPage(1)
             } else {
-                ctrl.form[property] = flight[property]
-                ctrl.example[property] = flight[property]
-                ctrl.gotoPage(1)
+                ctrl.search.form[property] = flight[property]
+                ctrl.search.example[property] = flight[property]
+                ctrl.refreshPage(1)
             }
         }
-        ctrl.clickHeading = function(property) {
-            if (ctrl.sort.property == property) {
-                ctrl.sort.direction = ctrl.sort.direction == "ASC" ? "DESC" : "ASC"
+        ctrl.headingClicked = function(property) {
+            if (ctrl.pageable.sort.property == property) {
+                var direction = ctrl.pageable.sort.direction
+                ctrl.pageable.sort.direction = direction == "ASC" ? "DESC" : "ASC"
             } else {
-                ctrl.sort.property = property
-                if (ctrl.sort.property == "startTime" || ctrl.sort.property == "endTime") {
-                    ctrl.sort.direction = "DESC"
+                if (property == "startTime" || property == "endTime") {
+                    ctrl.pageable.sort.direction = "DESC"
                 } else {
-                    ctrl.sort.direction = "ASC"
+                    ctrl.pageable.sort.direction = "ASC"
                 }
+                ctrl.pageable.sort.property = property
             }
             ctrl.refresh()
         }
-        ctrl.gotoPage = function(page) {
-            ctrl.page = page
+        ctrl.refreshPage = function(pageNumber) {
+            ctrl.pageable.pageNumber = pageNumber
             ctrl.refresh()
         }
         ctrl.refresh = function() {
-            var url = "/api/flights/?page=" + (ctrl.page - 1)
-            if (ctrl.sort && ctrl.sort.property && ctrl.sort.direction) {
-                var sortParam = "sort=" + ctrl.sort.property + "," + ctrl.sort.direction
-                url += "&" + sortParam
+            var url = "/api/flights/?page=" + (ctrl.pageable.pageNumber - 1)
+            if (ctrl.pageable.sort) {
+                var sort = ctrl.pageable.sort
+                if (sort.property && sort.direction) {
+                    var sortParam = "sort=" + sort.property + "," + sort.direction
+                    url += "&" + sortParam
+                }
             }
-            $http.post(url, ctrl.example)
+            $http.post(url, ctrl.search.example)
                 .then(function(response) {
                     ctrl.content = response.data.content
                     ctrl.durationTotals = response.data.durationTotals
--- src/main/resources/static/js/search/search.component.js
+++ src/main/resources/static/js/search/search.component.js
@@ -8,15 +8,15 @@ angular.module("search").component("search", {
     controller: function() {
         var ctrl = this
         ctrl.clear = function() {
-            ctrl.parent.form = {}
-            ctrl.parent.example = {}
+            ctrl.parent.search.form = {}
+            ctrl.parent.search.example = {}
             ctrl.exampleChanged()
         }
         ctrl.search = function() {
-            ctrl.parent.example.aircraft = ctrl.parent.form.aircraft
-            ctrl.parent.example.callsign = ctrl.parent.form.callsign
-            ctrl.parent.example.destination = ctrl.parent.form.destination
-            ctrl.parent.example.origin = ctrl.parent.form.origin
+            ctrl.parent.search.example.aircraft = ctrl.parent.search.form.aircraft
+            ctrl.parent.search.example.callsign = ctrl.parent.search.form.callsign
+            ctrl.parent.search.example.destination = ctrl.parent.search.form.destination
+            ctrl.parent.search.example.origin = ctrl.parent.search.form.origin
             ctrl.exampleChanged()
         }
     },
