eff541856d12afa557ad2b123bf105d1da851469
Richard Senior <richard.j.senior@gmail.com>
2018-02-13 20:57:17 +0000

    Pager now has a cap on the number of displayed cells (Issue #2)
     * Gaps are indicated with an ellipsis.
     * Allow up to three digits in page balloon.

--- src/main/resources/static/js/pager/pager.component.js
+++ src/main/resources/static/js/pager/pager.component.js
@@ -13,11 +13,35 @@ angular.module("pager").component("pager", {
             }
         }
         ctrl.pages = function() {
+            var target = 12
             pages = []
+            gaps = [0, 0]
             for (var i = 1; i <= ctrl.of; ++i) {
                 pages.push(i)
             }
-            return pages
+            if (pages.length <= target) {
+                return pages
+            }
+            while (pages.length + gaps[0] + gaps[1] > target) {
+                var l = pages[1]
+                var r = pages[pages.length - 2]
+                if (Math.abs(l - ctrl.page) > Math.abs(r - ctrl.page)) {
+                    pages.splice(1, 1)
+                    gaps[0] = 1
+                } else {
+                    pages.splice(pages.length - 2, 1)
+                    gaps[1] = 1
+                }
+            }
+            var gapped = []
+            var spacer = -1
+            for (var i = 0; i < pages.length; ++i) {
+                gapped.push(pages[i])
+                if (i + 1 < pages.length && pages[i + 1] - pages[i] > 1) {
+                    gapped.push(spacer--)
+                }
+            }
+            return gapped
         }
     },
     templateUrl: "js/pager/pager.template.html"
