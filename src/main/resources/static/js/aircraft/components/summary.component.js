83acfb3fdd1399d02c47b69514ef7f210d4ce8d2
Richard Senior <richard.j.senior@gmail.com>
2018-02-16 13:30:18 +0000

    Create aircraft summary page

---
+++ src/main/resources/static/js/aircraft/components/summary.component.js
@@ -0,0 +1,20 @@
+angular.module("aircraft").component("summary", {
+    bindings: {
+        content: "<",
+        durationTotals: "<",
+        onClickColumn: "&",
+        onClickHeading: "&"
+    },
+    controller: function() {
+        var ctrl = this
+        ctrl.clickColumn = function(flight, property) {
+            ctrl.onClickColumn({flight: flight, property: property})
+        }
+        ctrl.clickHeading = function(property) {
+            ctrl.onClickHeading({property: property})
+        }
+    },
+    templateUrl: "js/aircraft/components/summary.template.html"
+});
+
+
--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -1,6 +1,11 @@
-angular.module("application", ["core", "flightrecord", "ngRoute"])
-angular.module("application").config(function ($routeProvider) {
+angular.module("application", ["ngRoute", "aircraft", "flightrecord"])
+angular.module("application").config(function ($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
     $routeProvider
         .when("/", {template: "<flightrecord></flightrecord>"})
+        .when("/aircraft", {template: "<aircraft></aircraft>"})
         .otherwise("/");
 });
