f2251f3cb4b867ba708b68882cc7f0b0d0042499
Richard Senior <richard.j.senior@gmail.com>
2018-02-19 19:18:43 +0000

    Turn off html5 mode to allow manual page refresh

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -1,9 +1,6 @@
 angular.module("application", ["ngRoute", "aircraft", "flightrecord"])
 angular.module("application").config(function ($locationProvider, $routeProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
+    $locationProvider.html5Mode(false);
     $routeProvider
         .when("/", {template: "<flightrecord></flightrecord>"})
         .when("/aircraft", {template: "<aircraft></aircraft>"})
