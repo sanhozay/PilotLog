c478ded8a9b3d9e32ee0f4e9e42c89038642abc1
Richard Senior <richard.j.senior@gmail.com>
2018-02-20 11:46:24 +0000

    Remove redundant options from aircraft summary template

--- src/main/resources/static/js/aircraft/components/summary.component.js
+++ src/main/resources/static/js/aircraft/components/summary.component.js
@@ -2,14 +2,10 @@ angular.module("aircraft").component("summary", {
     bindings: {
         content: "<",
         totals: "<",
-        onClickColumn: "&",
         onClickHeading: "&"
     },
     controller: function() {
         var ctrl = this
-        ctrl.clickColumn = function(flight, property) {
-            ctrl.onClickColumn({flight: flight, property: property})
-        }
         ctrl.clickHeading = function(property) {
             ctrl.onClickHeading({property: property})
         }
