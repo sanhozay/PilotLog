33f789afc582405afe76ad5ed9fda7637f356539
Richard Senior <richard.j.senior@gmail.com>
2018-02-27 20:49:10 +0000

    Force test profile when testing flight repository
    This avoids running data-dev.sql if the application is being worked on
    using the development profile.

--- src/test/java/org/flightgear/pilotlog/domain/FlightRepositoryTest.java
+++ src/test/java/org/flightgear/pilotlog/domain/FlightRepositoryTest.java
@@ -24,6 +24,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
@@ -34,6 +35,7 @@ import static org.flightgear.pilotlog.domain.FlightStatus.COMPLETE;
 import static org.flightgear.pilotlog.domain.FlightStatus.INVALID;
 import static org.flightgear.pilotlog.domain.FlightStatus.NEW;
 
+@ActiveProfiles("test")
 @DataJpaTest
 @RunWith(SpringRunner.class)
 @SuppressWarnings("javadoc")
