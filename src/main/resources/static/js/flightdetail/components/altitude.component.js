32f4afc343c04008b2ec8e074d51fa071e31bf6c
Richard Senior <richard.j.senior@gmail.com>
2018-03-17 16:43:11 +0000

    Switch to chart.js

--- src/main/resources/static/js/flightdetail/components/altitude.component.js
+++ src/main/resources/static/js/flightdetail/components/altitude.component.js
@@ -20,19 +20,40 @@ angular.module("flightdetail").component("altitude", {
                         altitudes.push(altitude);
                         minutes.push(i / 6);
                     }
-                    var data = {
-                        labels: minutes,
-                        series: [altitudes]
-                    };
-                    var options = {
-                        axisX: {
-                            type: Chartist.AutoScaleAxis,
-                            onlyInteger: true,
-                            scaleMinSpace: 40,
-                            showGrid: false,
+                    var ctx = document.getElementById("altitude").getContext("2d");
+                    var chart = new Chart(ctx, {
+                        // The type of chart we want to create
+                        type: "line",
+
+                        // The data for our dataset
+                        data: {
+                            labels: minutes,
+                            datasets: [{
+                                label: "Altitude (ft)",
+                                borderColor: "rgb(0, 0, 128)",
+                                fill: false,
+                                pointRadius: 0,
+                                data: altitudes
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                xAxes: [{
+                                    gridLines: {
+                                        display: false
+                                    },
+                                    ticks: {
+                                        callback: function(value, index, values) {
+                                            if (index % 5 == 0) {
+                                                return value;
+                                            }
+                                            return null;
+                                        }
+                                    }
+                                }]
+                            }
                         }
-                    };
-                    var chart = new Chartist.Line(".ct-chart", data, options);
+                    });
                 });
         }
     },
